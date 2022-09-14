8dc4e289801bcc65848d9e43b7f191669307b55d
Ankita Singhal <ankitamk@gmail.com>
2022-09-14 18:12:06 +0000

    Finish related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -4,6 +4,8 @@ from blog.api.serializers import PostSerializer
 from blog.models import Post
 from blog.api.permissions import AuthorModifyOrReadOnly
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+from blango_auth.models import User
+from blog.api.serializers import PostSerializer, UserSerializer
 class PostList(generics.ListCreateAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
@@ -13,3 +15,8 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
   permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+
+class UserDetail(generics.RetrieveAPIView):
+  lookup_field = "email"
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
